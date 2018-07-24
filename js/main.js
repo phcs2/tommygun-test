e6edbf5b4b17e2c90a974aa14e95c22b39c45659
Andrey Kolembet <phcs2@yandex.ru>
2018-07-24 14:51:05 +0300

    Added product count functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function(){
   initCarousel();
+  initCount();
 });
 
 function initCarousel() {
@@ -19,4 +20,31 @@ function initCarousel() {
     centerMode: true,
     focusOnSelect: true
   });
+}
+
+function initCount() {
+  var input = document.querySelector('.product__count-input');
+  var minusBtn = document.querySelector('.product__count-minus');
+  var plusBtn = document.querySelector('.product__count-plus');
+  minusBtn.addEventListener('click', decreaseCount);
+  plusBtn.addEventListener('click', increaseCount);
+  input.addEventListener('input', validateCount);
+  input.addEventListener('blur', checkEmptyCount);
+
+  function decreaseCount() {
+    if (input.value < 2) return;
+    input.value = +input.value - 1;
+  }
+
+  function increaseCount() {
+    input.value = +input.value + 1;
+  }
+
+  function validateCount() {
+    input.value = input.value.match(/[1-9]\d{0,2}/);
+  }
+
+  function checkEmptyCount() {
+    if (!input.value) input.value = 1;
+  }
 }
